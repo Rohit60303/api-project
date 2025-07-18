a57f1ff915458c8a7017a7fbd9e6f80d446a5f53
Rohit60303 <rohitvarmab448@gmail.com>
2025-07-18 23:20:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname)));
 app.get('*', (req, res) => 
 {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'));
 });
 app.listen(PORT, () => 
 {
